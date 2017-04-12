031a4c3b2ae0f71dc1f820a8fb26d34cc4038d9c
kkohl <kkohl@iastate.edu>
2017-04-12 15:49:36 -0500

    Created framework for project

---
+++ src/DynamicProgramming.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Ken Kohl on 4/12/2017.
+ */
+public class DynamicProgramming {
+
+
+    public static ArrayList<Integer> minCostVC(int[][] M) {
+        return null;
+    }
+}
---
+++ src/ImageProcessor.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Ken Kohl on 4/12/2017.
+ */
+public class ImageProcessor {
+    public void ImageProcessor(String imageFile) {
+
+    }
+
+    public static Picture reduceWidth(double x) {
+        return null;
+    }
+
+}
