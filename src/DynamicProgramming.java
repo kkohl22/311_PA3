b2b4d90df4a4f305a9291e7e40a369b4d84ee26b
kkohl <kkohl@iastate.edu>
2017-04-13 18:06:13 -0500

    added jdoc to methods

--- src/DynamicProgramming.java
+++ src/DynamicProgramming.java
@@ -5,8 +5,22 @@ import java.util.ArrayList;
  */
 public class DynamicProgramming {
 
-
+    /**
+     *
+     * @param M
+     * @return a min-cost vertical cut.
+     */
     public static ArrayList<Integer> minCostVC(int[][] M) {
+        return null;
+    }
+
+    /**
+     *
+     * @param x  string of length n.
+     * @param y string of length m
+     * @return d returns a string z (obtained by inserting $ at n − m indices in y) such that AlignCost(x, z) ≤ AlignCost(x, y) over all possible y
+     */
+    public static String stringAlignment(String x, String y) {
         return null;
     }
 }
--- src/ImageProcessor.java
+++ src/ImageProcessor.java
@@ -2,10 +2,19 @@
  * Created by Ken Kohl on 4/12/2017.
  */
 public class ImageProcessor {
+    /**
+     *
+     * @param imageFile
+     */
     public void ImageProcessor(String imageFile) {
 
     }
 
+    /**
+     *
+     * @param x
+     * @return
+     */
     public static Picture reduceWidth(double x) {
         return null;
     }
